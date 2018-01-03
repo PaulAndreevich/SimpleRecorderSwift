824efc16ee7edf36d65deb84b98b6f0a1be70561
Neon <abanshin@gmail.com>
2018-01-03 19:20:40 +0300

    Initial Commit

---
+++ RecorderSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RecorderSwift
+//
+//  Created by Boss on 03/01/2018.
+//  Copyright © 2018 Boss. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
