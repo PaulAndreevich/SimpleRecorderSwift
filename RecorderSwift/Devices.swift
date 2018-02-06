aff5cabb1a118602d8f85f181f01ffe7a0662d29
Neon <abanshin@gmail.com>
2018-02-06 23:18:18 +0300

    Created simple audio recorder from internal mic on mac AVFoundation

---
+++ RecorderSwift/Devices.swift
@@ -0,0 +1,28 @@
+//
+//  Devices.swift
+//  RecorderSwift
+//
+//  Created by Boss on 23/01/2018.
+//  Copyright © 2018 Boss. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+import AVKit
+import AVFoundation
+
+//Вывод всех устройств, записывающих аудио
+class Device {
+    
+    /*
+    var captureSession = AVCaptureSession()
+    captureSession.sessionPreset = AVCaptureSession.Preset.low
+
+    let devicelist = AVCaptureDevice.devices(for: AVMediaType.audio)
+    
+    if devicelist.isEmpty {
+        print("is empty")
+    }
+    //print(devices)*/
+
+}
--- RecorderSwift/ViewController.swift
+++ RecorderSwift/ViewController.swift
@@ -8,10 +8,31 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+import AVFoundation
+import AVKit
 
+class ViewController: NSViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate{
+
+    @IBOutlet weak var startRecordingBTN: NSButton!
+    @IBOutlet weak var stopRecordingBTN: NSButton!
+    @IBOutlet weak var playRecordBTN: NSButton!
+    @IBOutlet weak var stopPlayingBTN: NSButton!
+    
+    var soundRecorder : AVAudioRecorder!
+    var soundPlayer : AVAudioPlayer!
+    
+    
+    //let devices = AVCaptureDevice.devices(for: AVMediaType.audio)
+   // var capdev : AVCaptureDevice
+    
+    //capdev.
+    
+    
+    var fileName = "audioFile.m4a"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupRecorder()
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +43,107 @@ class ViewController: NSViewController {
         }
     }
 
+    func setupRecorder(){
+        
+        let recordSettings = [AVFormatIDKey: kAudioFormatAppleLossless,
+                              AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
+                              AVEncoderBitRateKey: 320000,
+                              AVNumberOfChannelsKey: 2,
+                              AVSampleRateKey: 44100.0 ] as [String : Any]
+        
+        //var error : NSError?
+        
+        let docDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let fileURL = docDirURL.appendingPathComponent(fileName)
+        
+        soundRecorder = try! AVAudioRecorder.init(url: fileURL, settings: recordSettings)
+        
+        soundRecorder.delegate = self
+        soundRecorder.prepareToRecord()
+        
+       /* if let err = error{
+            NSLog("Something Wrong")
+        }
+        else {
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+        }*/
+    }
+    
+    func getCacheDirectory() -> String {
+        
+        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true) as! [String]
+        
+        return paths[0]
+    }
+    
+    func getFileURL() -> NSURL {
+       
+        //let path = (getCacheDirectory() as NSString).stringByAppendingPathComponent(fileName)
+   
+        let path = (getCacheDirectory() as NSString).appendingPathComponent(fileName)
+        
+        let filePath = NSURL(fileURLWithPath: path)
+        
+        return filePath
+    }
+    
+    @IBAction func startRecording(_ sender: Any) {
+        soundRecorder.record()
+        startRecordingBTN.isEnabled = false
+        playRecordBTN.isEnabled = false
+        stopPlayingBTN.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        soundRecorder.stop()
+        startRecordingBTN.isEnabled = true
+        playRecordBTN.isEnabled = false
+        //stopPlayingBTN.isEnabled = false
+    }
+    
+    @IBAction func playRecord(_ sender: Any) {
+        startRecordingBTN.isEnabled = false
+        preparePlayer()
+        soundPlayer.play()
+        
+    }
+    @IBAction func stopPlaying(_ sender: Any) {
+        stopPlayingBTN.isEnabled = false
+    }
+    
+    func preparePlayer(){
+        
+        let docDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let fileURL = docDirURL.appendingPathComponent(fileName)
+        
+        soundPlayer = try! AVAudioPlayer.init(contentsOf : fileURL)
+        soundPlayer.delegate  = self
+        soundPlayer.prepareToPlay()
+        soundPlayer.volume = 1.0
+        
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        playRecordBTN.isEnabled = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        startRecordingBTN.isEnabled = true
+    }
+    
+    
+    
+    
 
+
+    
 }
+
+
+
+
+
+
+
 
